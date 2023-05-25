b6093315fa61e0b1d4bdd36147388f18e393aa7a
Ali shah Erfan <129274659+Ali-shah-erfan@users.noreply.github.com>
2023-05-26 01:21:18 +0430

    Add files via upload

---
+++ lib/IconContent.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'constants.dart';
+
+class IconContent extends StatelessWidget {
+  final IconData iconData;
+  final String label; 
+
+     IconContent({required this.iconData, required this.label});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+       Icon(
+        IconData,
+       size:65,
+       color: Color(0xFF868892),
+       ),
+       SizedBox(
+        height:10,
+        ),
+       Text(
+        'label',
+        style:labelstyle,
+       ),
+      ],
+
+    );
+  }
+}
